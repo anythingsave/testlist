450438ca021924e4ef02cecbba73db245e2875c8
anythingsave <89069873+anythingsave@users.noreply.github.com>
2022-08-25 01:38:12 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+{
+
+    document.oncontextmenu = function () {return false;}
+
+
+
+    let ready = (callbackFunc) => {
+        if (document.readyState !== 'loading') {
+            callbackFunc();
+        } else {
+            document.addEventListener('DOMContentLoaded', callbackFunc);
+        }
+    }
+
+    ready(() => {
+        let video = videojs('video1', {
+            width: 734, // 幅
+            height: 413, // 高さ
+            autoplay: false, // 自動再生
+            loop: false, // ループ再生
+            controls: true, // コントロール制御表示
+            preload: 'auto', // 読み込み制御
+        });
+        video.src({
+            type: 'application/dash+xml',
+            src: 'stream/mpd/dash.mpd',
+            keySystemOptions: [{
+                name: 'com.widevine.alpha',
+                options: {
+                    serverURL: 'https://license.uat.widevine.com/cenc/getcontentkey/widevine_test'
+                }
+            }]
+        });
+
+        video.on(['loadstart', 'loadedmetadata', 'loadeddata', 'play', 'playing', 'pause', 'suspend', 'seeking', 'seeked', 'waiting', 'canplay', 'canplaythrough', 'ratechange', 'ended', 'emptied', 'error', 'abort'], (e) => {
+            console.log(`EVENT: ${e.type}`);
+        });
+        video.on('loadeddata', () => {
+            console.debug('########## VideoInfo [start] ##########');
+            console.debug(`>> source: ${video.currentSrc()}`);
+            console.debug(`>> duration: ${video.duration()}`);
+            console.debug(`>> videoSize(WxH): ${video.videoWidth()}px x ${video.videoHeight()}px`);
+            console.debug(`>> readyState: ${video.readyState()}`);
+            console.debug(`>> networkState: ${video.networkState()}`);
+            console.debug('########## VideoInfo [end] ##########');
+        });
+    });
+}
