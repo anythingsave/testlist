5f59251459becb80b5e5d1443189e8a67031377d
anythingsave <89069873+anythingsave@users.noreply.github.com>
2022-08-25 12:29:36 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -22,8 +22,8 @@
             preload: 'auto', // 読み込み制御
         });
         video.src({
-            type: 'application/dash+xml',
-            src: 'stream/mpd/master.mpd',
+            type: 'application/vnd.apple.mpegurl',
+            src: 'stream/m3u8/master.m3u8',
             keySystemOptions: [{
                 name: 'com.widevine.alpha',
                 options: {
